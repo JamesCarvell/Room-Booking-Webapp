bff10f468dfe4292086534c2b39a39839b202721
James <jamesrcarvell@gmail.com>
2023-08-24 20:32:08 -0300

    Changes to be committed: 	modified:   __pycache__/bookings.cpython-311.pyc 	modified:   bookings.py 	modified:   db.json 	new file:   static/js/calendar_script.js 	modified:   templates/index.html

--- bookings.py
+++ bookings.py
@@ -2,9 +2,10 @@
 over whole file. """
 import json
 import collections
+import datetime
 
 
-def read_bookings(database):
+def read_bookings(database) -> dict:
     with open(database, "r", encoding="utf-8") as db:
         data_dict = json.load(db)
         return data_dict
@@ -23,7 +24,10 @@ def expand_bookings(database) -> list:
     for room in bookings_dict.keys():
         for booking_date in bookings_dict[room].keys():
             for day in range(bookings_dict[room][booking_date][1]):
-                expanded_bookings.append(int(booking_date) + day)
+                year = booking_date[:4]
+                month = booking_date[4:6]
+                day = booking_date[6:]
+                expanded_bookings.append(datetime.date(booking_date) + datetime.timedelta(days=day))
     return expanded_bookings
 
 
@@ -31,3 +35,5 @@ def room_availability(expanded_bookings: list, room_count: int) -> list:
     count_by_date = collections.Counter(expanded_bookings)
     unavailable = [booking_date for booking_date in count_by_date.keys() if count_by_date[booking_date] >= room_count]
     return(unavailable)
+
+print(expand_bookings("db.json"))
